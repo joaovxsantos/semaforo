a888e63a79d9c31b51d345c3e53d66c3ef9d280a
João Vitor <joaovitor55670@gmail.com>
2022-07-02 16:05:04 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let img = document.getElementsByTagName("img")[0]
-let IndexColor = 0
-let Intervalo = null
-let botao = document.getElementById("botoes")
+let img = document.getElementsByTagName("img")[0] //pegando a imagem
+let IndexColor = 0 //criando variável com valor 0
+let Intervalo = null //variavel com valor nulo
+let botao = document.getElementById("botoes") //pegando a div botoes
 
 
+//aq é a função que passei no evento click para o "botao"
 function luzes(e) {
-    stop()
-    turn[e.target.id]()
+    stop() //aq to pegando uma função
+    turn[e.target.id]() //aq eu coloco a variável turn, com o elemento, target e id
 
 }
 
+//aq um objeto com o que cada cor vai receber
 let turn = {
     red: () => img.src = "./images/vermelho.png",
     yellow: () => img.src = "./images/amarelo.png",
@@ -17,6 +19,8 @@ let turn = {
     automatic: () => Intervalo = setInterval(change, 1000)
 }
 
+
+//aq é a função pro automatic, pra ficar fazendo rodizio de cores
 function change() {
     let colors = ['red', 'yellow', 'green']
     let color = colors[IndexColor]
@@ -24,17 +28,18 @@ function change() {
     another()
 }
 
-function stop() {
-    clearInterval(Intervalo)
-}
-
-
+//aq é pra incrementar e fazer o rodizio de cores
 function another() {
     if (IndexColor < 2) {
         IndexColor++
     } else {
         IndexColor = 0;
     }
+}
+
+//aq é pra limpar o rodizio de cores quando clicar em outro botao
+function stop() {
+    clearInterval(Intervalo)
 }
 
 botao.addEventListener("click", luzes)
