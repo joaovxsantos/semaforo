2008e5fdb6eaf99c8b9881f74431cf92106eb4a9
João Vitor <joaovitor55670@gmail.com>
2022-07-02 15:47:32 -0300

    commit

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 let img = document.getElementsByTagName("img")[0]
-
-
+let IndexColor = 0
+let Intervalo = null
 let botao = document.getElementById("botoes")
 
 
-function trafficlight( event ){
-   turnOn[event.target.id]();
+function luzes(e) {
+    stop()
+    turn[e.target.id]()
+
 }
 
-
-let turnOn = {
+let turn = {
     red: () => img.src = "./images/vermelho.png",
     yellow: () => img.src = "./images/amarelo.png",
-    green: ()=> img.src = "./images/verde.png"
+    green: () => img.src = "./images/verde.png",
+    automatic: () => Intervalo = setInterval(change, 1000)
+}
+
+function change() {
+    let colors = ['red', 'yellow', 'green']
+    let color = colors[IndexColor]
+    turn[color]()
+    another()
+}
+
+function stop() {
+    clearInterval(Intervalo)
 }
 
 
-botao.addEventListener("click", trafficlight)
+function another() {
+    if (IndexColor < 2) {
+        IndexColor++
+    } else {
+        IndexColor = 0;
+    }
+}
+
+botao.addEventListener("click", luzes)
